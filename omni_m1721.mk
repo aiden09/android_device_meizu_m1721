d324fdaad5c4b74bb3a9fdd043852c0caeb45e99
root <root@kyllene.net>
2018-09-10 15:47:59 +0300

    add decryption support and cleanup

--- omni_m1721.mk
+++ omni_m1721.mk
@@ -23,8 +23,8 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+#PRODUCT_COPY_FILES += \
+#    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_m1721
